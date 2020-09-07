23e3e7da96df892724faf0c503b74be584e4533b
pratikkacha <70179445+pratikkacha@users.noreply.github.com>
2020-09-07 12:29:48 -0700

    Add files via upload

---
+++ Workbook 1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[32]:
+
+
+import pandas as pd
+Location = "https://www2.census.gov/library/publications/2011/compendia/usa-counties/excel/BNK01.xls"
+df = pd.read_excel(Location)
+df.head()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
