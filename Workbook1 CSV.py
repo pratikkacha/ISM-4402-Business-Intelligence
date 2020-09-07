48a2c3062956a128633669bccfa2983fb236e76e
pratikkacha <70179445+pratikkacha@users.noreply.github.com>
2020-09-07 12:43:31 -0700

    Add files via upload

---
+++ Workbook1 CSV.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+import pandas as pd
+Location = "http://censusdata.ire.org/06/all_040_in_06.P1.csv"
+df = pd.read_csv(Location)
+df.head()
+
+
+# In[ ]:
+
+
+
+
